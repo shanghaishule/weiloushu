da9d4ee623e21a945654dd4535677266fed482f8
cairne <1402612547@qq.com>
2014-06-12 09:17:58 +0800

    品牌和项目介绍页面

---
+++ SRC/Lanrain/Lib/Action/Mobil/BrandAction.class.php
@@ -0,0 +1,11 @@
+<?php
+class BrandAction extends BaseAction{
+	//关注回复
+	public function index(){
+		//echo "d ads as df";
+		$this->display();
+	}
+	public function project(){
+		$this->display();
+	}
+}
--- SRC/data/conf/db.php
+++ SRC/data/conf/db.php
@@ -3,9 +3,9 @@ return array (
   'DB_TYPE' => 'mysql',
   'DB_HOST' => 'localhost',
   'DB_PORT' => '3306',
-  'DB_NAME' => 'wxshoppingmall',
+  'DB_NAME' => 'weiloushu',
   'DB_USER' => 'root',
-  'DB_PWD' => 'nopassword',
+  'DB_PWD' => '',
   'DB_PREFIX' => 'tp_',
 );
 ?>
